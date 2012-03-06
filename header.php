411b66278f52ae300ff842d3dc2be6d8082bda2e
Antoine Neveux <antoine.neveux@gmail.com>
2012-03-06 13:53:26 +0100

    Updated footer's style to remove some margin

--- header.php
+++ header.php
@@ -12,12 +12,13 @@
 	
 	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
 	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	
 	<!-- Included Foundation CSS Files -->
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/foundation.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/app.css">
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	<?php if (is_child_theme()) { ?>
 	<!-- Include Child theme CSS file -->
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
